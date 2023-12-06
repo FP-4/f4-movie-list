f42a0595a36c7b9306705627b63991ade4666159
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-12-06 12:54:18 +0700

    chore: remove apikey and store to env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,8 +4,7 @@ const apiInstance = axios.create({
 	baseURL: "https://api.themoviedb.org/3",
 	headers: {
 		Accept: "application/json",
-		Authorization:
-			"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NmRhMzgwMjdjOGM4NWYwNzA4MGJjNGI2Y2YwOWM3MyIsInN1YiI6IjYwZTEzMmQxYjY4NmI5MDA0NmVhMzdkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ghgtmpsT2cjO0Ri4M49RQAIvfBj3JU4TY4PCHFi6yp0",
+		Authorization: `Bearer ${import.meta.env.VITE_TMDB_APIKEY as string}`,
 	},
 });
 
