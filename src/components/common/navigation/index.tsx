40f21610cdd06831874cb244b58d2b30a693f04e
zaki fauzan <zakifauzanrabb@gmail.com>
2023-12-06 18:03:43 +0800

    add genre at nav

--- src/components/common/navigation/index.tsx
+++ src/components/common/navigation/index.tsx
@@ -1,58 +1,74 @@
-import WebLogo from "@/assets/logo.png";
-import clsx from "clsx";
+import { useState, useEffect, useMemo } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate, useLocation } from "react-router-dom";
-import React, { useEffect, useState, useMemo } from "react";
+import clsx from "clsx";
 import { Search } from "@/components/common";
+import WebLogo from "@/assets/logo.png";
+import { useMovieGenre } from "@/providers/movie";
 
 export const Navigation = ({ customClass }: { customClass?: string }) => {
-	const navigate = useNavigate();
-	const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { data: genresData } = useMovieGenre();
+  const queryParams = useMemo(() => new URLSearchParams(location.search), [location.search]);
+  const [searchQuery, setSearchQuery] = useState(queryParams.get("q") || "");
+  const [isGenreOpen, setIsGenreOpen] = useState(false);
 
-	const queryParams = useMemo(
-		() => new URLSearchParams(location.search),
-		[location.search]
-	);
-	const [searchQuery, setSearchQuery] = useState(queryParams.get("q") || "");
+  const handleOnChangeSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
 
-	const handleOnChangeSearch = (event: React.ChangeEvent<HTMLInputElement>) =>
-		setSearchQuery(event.target.value);
+  const handleNavigate = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    queryParams.delete("q");
+    if (searchQuery.trim() !== "") {
+      queryParams.set("q", searchQuery);
+    }
+    navigate(`/search?${queryParams.toString()}`);
+  };
 
-	const handleNavigate = (event: React.MouseEvent<HTMLDivElement>) => {
-		event.preventDefault();
+  useEffect(() => {
+    const queryParam = queryParams.get("q") || "";
+    setSearchQuery(queryParam);
+  }, [queryParams]);
 
-		queryParams.delete("q");
-		if (searchQuery.trim() !== "") {
-			queryParams.set("q", searchQuery);
-		}
-		navigate(`/search?${queryParams.toString()}`);
-	};
-
-	useEffect(() => {
-		const queryParam = queryParams.get("q") || "";
-		setSearchQuery(queryParam);
-	}, [queryParams]);
-
-	return (
-		<header className={clsx("py-4 text-white  w-full z-10", customClass)}>
-			<nav className="w-full md:max-w-7xl mx-auto flex justify-between items-center px-5">
-				<Link to="/" className="w-10 h-10">
-					<img src={WebLogo} alt="Logo" />
-				</Link>
-				<Search
-					searchQuery={searchQuery}
-					handleNavigate={handleNavigate}
-					handleOnChangeSearch={handleOnChangeSearch}
-				/>
-				<div className="hidden gap-4">
-					<Link to="#" className="py-3 px-4 font-semibold text-brand-grey/200">
-						Movies
-					</Link>
-					<Link to="#" className="py-3 px-4 font-semibold text-brand-grey/200">
-						TV Show
-					</Link>
-				</div>
-			</nav>
-		</header>
-	);
+  return (
+    <header className={clsx("py-4 text-white w-full z-10", customClass)}>
+      <nav className="w-full md:max-w-7xl mx-auto flex justify-between items-center px-5">
+        <Link to="/" className="w-10 h-10">
+          <img src={WebLogo} alt="Logo" />
+        </Link>
+        <Search
+          searchQuery={searchQuery}
+          handleNavigate={handleNavigate}
+          handleOnChangeSearch={handleOnChangeSearch}
+        />
+		<div className="relative"             
+			onMouseEnter={() => setIsGenreOpen(true)}
+			onMouseLeave={() => setIsGenreOpen(false)}>
+		<button className="font-semibold text-brand-grey/200 focus:outline-none">
+			Genre
+		</button>
+		{isGenreOpen && (
+			<div className="absolute  bg-white w-max rounded-md shadow-lg grid grid-cols-3">
+			{genresData?.genres.map((item)=>(
+			<Link to="#" className="text-center block px-4 py-2 text-brand-grey/200 hover:bg-gray-100 hover:text-brand-grey/500">
+				{item.name}
+			</Link>
+			))}
+			</div>
+		)}
+		</div>
+        <div className="gap-4">
+			<Link to="#" className="py-3 px-4 font-semibold text-brand-grey/200">
+			Movies
+			</Link>
+			
+			<Link to="#" className="py-3 px-4 font-semibold text-brand-grey/200">
+			TV Show
+			</Link>
+        </div>
+      </nav>
+    </header>
+  );
 };
